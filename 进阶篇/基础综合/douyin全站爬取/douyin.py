7d2c4b0c07af2a90058bd585e5cbc49e8f3f7b02
xishandong <xishandong296@gmail.com>
2023-09-25 13:51:04 +0800

    修复抖音多线程下载评论的bug

--- 进阶篇/基础综合/douyin全站爬取/douyin.py
+++ 进阶篇/基础综合/douyin全站爬取/douyin.py
@@ -2,6 +2,7 @@ import csv
 import json
 import os
 import time
+from threading import Lock, Condition
 from concurrent.futures import ThreadPoolExecutor
 from typing import Union
 from urllib.parse import urlencode
@@ -22,7 +23,7 @@ class Douyin:
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
         }
         self.cookies = {
-            "sid_tt": "",
+            "sid_tt": "585fa50b2957def5b8679dda6e5afbc7",
         }
         # 访问所需的api接口
         self.api = [
@@ -33,8 +34,11 @@ class Douyin:
             'https://www.douyin.com/aweme/v1/web/general/search/single/',
             'https://www.douyin.com/aweme/v1/web/hot/search/list/',
             'https://www.douyin.com/aweme/v1/web/discover/search/',
-            'https://www.douyin.com/aweme/v1/web/hot/search/list/'
         ]
+        # 线程锁相关
+        self.write_in_progress = False
+        self.file_lock = Lock()
+        self.write_condition = Condition(lock=self.file_lock)
 
     # 用于发送请求，设置了5次重试，一般情况如果可以获取数据5次重试就足够了
     @retry(stop_max_attempt_number=5, wait_exponential_multiplier=1000, wait_exponential_max=20000)
@@ -63,11 +67,16 @@ class Douyin:
             'sec_user_id': sec_id,
         }
         # 用其他接口获取的用户信息
-        data = self.ajax_requests(1, params)
-        user = next(self.search_dir(data, 'user'), None)
+        try:
+            data = self.ajax_requests(1, params)
+        except json.JSONDecodeError:
+            logger.error(
+                f'超过五次服务器未返回信息，更新cookie或者检查加密参数是否错误，发送错误api编号{1}, params: {params}')
+            return None
+        user = next(self.__search_dir(data, 'user'), None)
         if user:
             logger.info('获取到用户信息')
-            return next(self.get_user_info(user), None)
+            return next(self.__get_user_info(user), None)
         return None
 
     # 通过用户加密的id获取用户发布的短视频或者图文帖子相关信息
@@ -84,14 +93,19 @@ class Douyin:
         continuations = [params]
         while continuations:
             continuation = continuations.pop()
-            data = self.ajax_requests(0, continuation)
+            try:
+                data = self.ajax_requests(0, continuation)
+            except json.JSONDecodeError:
+                logger.error(
+                    f'超过五次服务器未返回信息，更新cookie或者检查加密参数是否错误，发送错误api编号{0}, params: {continuation}')
+                return None
             # 模拟向下滑动
-            if next(self.search_dir(data, 'has_more'), None):
-                params['max_cursor'] = str(next(self.search_dir(data, 'max_cursor'), None))
+            if next(self.__search_dir(data, 'has_more'), None):
+                params['max_cursor'] = str(next(self.__search_dir(data, 'max_cursor'), None))
                 continuations.append(params)
-            for posts in self.search_dir(data, 'aweme_list'):
+            for posts in self.__search_dir(data, 'aweme_list'):
                 for post in posts:
-                    yield from self.get_post(post)
+                    yield from self.__get_post(post)
                     self.__sleep_time(1)
             self.__sleep_time()
 
@@ -108,27 +122,30 @@ class Douyin:
         continuations = [params]
         while continuations:
             continuation = continuations.pop()
-            data = self.ajax_requests(2, continuation)
+            try:
+                data = self.ajax_requests(2, continuation)
+            except json.JSONDecodeError:
+                logger.error(
+                    f'超过五次服务器未返回信息，更新cookie或者检查加密参数是否错误，发送错误api编号{2}, params: {continuation}')
+                return None
             # 模拟向下滑动
-            if next(self.search_dir(data, 'has_more'), None):
-                params['cursor'] = next(self.search_dir(data, 'cursor'), None)
+            if next(self.__search_dir(data, 'has_more'), None):
+                params['cursor'] = next(self.__search_dir(data, 'cursor'), None)
                 continuations.append(params)
             if not data.get('comments'):
-                logger.error(f'帖子:{id}, 么有评论哦~')
-                return False
+                logger.info(f'帖子:{id}, 么有评论哦~')
+                return None
             for comment in data.get('comments'):
-                yield from self.get_comment(comment)
-                self.__sleep_time(.5)
+                yield from self.__get_comment(comment)
                 # 模拟点击更多回复
                 if comment['reply_comment_total'] > 0:
                     more_comment_params = {
                         'aid': '6383',
                         'comment_id': comment['cid'],
                         'cursor': '0',
-                        'count': '3',
+                        'count': '10',
                     }
                     yield from self.more_comments(more_comment_params)
-                    self.__sleep_time(.5)
             self.__sleep_time()
 
     # 点击更多回复触发的事件
@@ -136,16 +153,19 @@ class Douyin:
         continuations = [params]
         while continuations:
             continuation = continuations.pop()
-            data = self.ajax_requests(3, continuation)
+            try:
+                data = self.ajax_requests(3, continuation)
+            except json.JSONDecodeError:
+                logger.error(
+                    f'超过五次服务器未返回信息，更新cookie或者检查加密参数是否错误，发送错误api编号{3}, params: {continuation}')
+                return None
             # 模拟点击更多回复
-            if next(self.search_dir(data, 'has_more'), None):
-                params['cursor'] = next(self.search_dir(data, 'cursor'), None)
+            if next(self.__search_dir(data, 'has_more'), None):
+                params['cursor'] = next(self.__search_dir(data, 'cursor'), None)
                 params['count'] = '10'
                 continuations.append(params)
             for comment in data.get('comments'):
-                yield from self.get_comment(comment)
-                self.__sleep_time(.5)
-            self.__sleep_time()
+                yield from self.__get_comment(comment)
 
     # 通过关键词定位短视频
     # sort_type: 0为综合，1为点赞量，2为新发布
@@ -169,37 +189,41 @@ class Douyin:
         continuations = [params]
         while continuations:
             continuation = continuations.pop()
-            data = self.ajax_requests(4, continuation)
+            try:
+                data = self.ajax_requests(4, continuation)
+            except json.JSONDecodeError:
+                logger.error(
+                    f'超过五次服务器未返回信息，更新cookie或者检查加密参数是否错误，发送错误api编号{4}, params: {continuation}')
+                return None
             # 模拟向下滑动
-            if next(self.search_dir(data, 'has_more'), None):
-                params['offset'] = next(self.search_dir(data, 'cursor'), None)
+            if next(self.__search_dir(data, 'has_more'), None):
+                params['offset'] = next(self.__search_dir(data, 'cursor'), None)
                 continuations.append(params)
             # 根据不同类型的帖子传入不同的值进行解析，如果有没见过的类型，会打印类型，用户可以自行通过类型补充在条件判断中
-            for posts in self.search_dir(data, 'data'):
+            for posts in self.__search_dir(data, 'data'):
                 for post in posts:
                     if post['type'] == 1:
-                        yield from self.get_post(post['aweme_info'])
+                        yield from self.__get_post(post['aweme_info'])
                     elif post['type'] == 16:
                         for _item in post['aweme_mix_info']['mix_items']:
-                            yield from self.get_post(_item)
+                            yield from self.__get_post(_item)
                     elif post['type'] == 60:
-                        for items in self.search_dir(post, 'hotspot_items'):
+                        for items in self.__search_dir(post, 'hotspot_items'):
                             for _item in items:
-                                yield from self.get_post(_item)
+                                yield from self.__get_post(_item)
                     elif post['type'] in [76, 77]:
                         pass
                     elif post['type'] == 4:
                         for user in post['user_list']:
                             if user.get('items'):
                                 for item in user.get('items'):
-                                    yield from self.get_post(item)
+                                    yield from self.__get_post(item)
                     elif post['type'] == 996:
-                        for items in self.search_dir(post, 'hotspot_items'):
+                        for items in self.__search_dir(post, 'hotspot_items'):
                             for _item in items:
-                                yield from self.get_post(_item)
+                                yield from self.__get_post(_item)
                     else:
                         logger.error(f'出现未知帖子类型(搜索页): {post}')
-                    self.__sleep_time(.5)
             self.__sleep_time()
 
     # 通过关键词搜索用户
@@ -217,21 +241,30 @@ class Douyin:
         continuations = [params]
         while continuations:
             continuation = continuations.pop()
-            data = self.ajax_requests(6, continuation)
+            try:
+                data = self.ajax_requests(6, continuation)
+            except json.JSONDecodeError:
+                logger.error(
+                    f'超过五次服务器未返回信息，更新cookie或者检查加密参数是否错误，发送错误api编号{6}, params: {continuation}')
+                return None
             # 模拟向下滑动
-            if next(self.search_dir(data, 'has_more'), None):
-                params['offset'] = next(self.search_dir(data, 'cursor'), None)
+            if next(self.__search_dir(data, 'has_more'), None):
+                params['offset'] = next(self.__search_dir(data, 'cursor'), None)
                 continuations.append(params)
             for user in data['user_list']:
-                yield from self.get_user_info(user.get('user_info'))
-                self.__sleep_time(.5)
+                yield from self.__get_user_info(user.get('user_info'))
             self.__sleep_time()
 
     # 获取当前热搜
     def get_hotSearch(self):
         logger.debug("开始搜索热搜")
-        res = self.ajax_requests(7, params={}).json()
-        word_list = next(self.search_dir(res, 'word_list'))
+        try:
+            res = self.ajax_requests(5, params={})
+        except json.JSONDecodeError:
+            logger.error(
+                f'超过五次服务器未返回信息，更新cookie或者检查加密参数是否错误，发送错误api编号{5}')
+            return None
+        word_list = next(self.__search_dir(res, 'word_list'))
         # 只爬取了热搜，如果想要其他信息可以自行找api接口
         for hotpoint in word_list:
             word = hotpoint.get('word')
@@ -244,19 +277,52 @@ class Douyin:
                 'event_time': event_time
             }
 
-    def save2csv(self, id):
+    def save_comment_to_csv_by_id(self, id):
+        def write_comments(is_write_headers, comment=None):
+            header = ['reply_id', 'comment_id', 'reply_to_reply_id', 'sec_uid', 'user', 'reply_to_user_sec_id',
+                      'reply_to_username', 'text', 'sticker', 'liked', 'reply_count', 'ip', 'create_time']
+            mode = 'a' if not is_write_headers else 'w'
+            with self.file_lock:
+                with open(f'{id}.csv', mode, encoding='utf-8', newline='') as fp:
+                    writer = csv.DictWriter(fp, header)
+                    if is_write_headers:
+                        writer.writeheader()
+                        logger.debug('写入表头了')
+                    if comment:
+                        writer.writerow(comment)
+
+            with self.file_lock:
+                # 等待写入权限
+                while self.write_in_progress:
+                    self.write_condition.wait()
+                # 占位
+                self.write_in_progress = True
+                with open(f'{id}.csv', mode, encoding='utf-8', newline='') as fp:
+                    writer = csv.DictWriter(fp, header)
+                    if is_write_headers:
+                        writer.writeheader()
+                    if comment:
+                        writer.writerow(comment)
+                # 完成写入，释放写入权限
+                self.write_in_progress = False
+                self.write_condition.notify_all()
+
         _items = self.get_comment_by_id(id)
-        header = ['reply_id', 'comment_id', 'reply_to_reply_id', 'sec_uid', 'user', 'reply_to_user_sec_id',
-                  'reply_to_username', 'text', 'sticker', 'liked', 'reply_count', 'ip', 'create_time']
-        fp = open(f'{id}.csv', 'w', encoding='utf-8', newline='')
-        writer = csv.DictWriter(fp, header)
-        writer.writeheader()
-        flag = 0
-        for _i in _items:
-            flag += 1
-            if flag % 50 == 0:
-                logger.info(f'写入{flag}条数据')
-            writer.writerow(_i)
+
+        is_first = True
+        # 使用多线程下载
+        with ThreadPoolExecutor(max_workers=16) as executor:
+            futures = []
+            for _i in _items:
+                if is_first and _i:
+                    write_comments(is_first)
+                    is_first = False
+                future = executor.submit(write_comments, is_first, _i)
+                futures.append(future)
+            for future in futures:
+                future.result()
+            executor.shutdown()
+        logger.info(f'帖子: {id}下载完毕...')
 
     def download_user_all_posts(self, user_id):
         user: dict = self.get_user(user_id)
@@ -284,28 +350,38 @@ class Douyin:
                 future.result()
             executor.shutdown()
 
-    @retry(stop_max_attempt_number=7, wait_exponential_multiplier=1000, wait_exponential_max=20000)
     def __download(self, _item, folder_path='./Lib'):
+        @retry(stop_max_attempt_number=7, wait_exponential_multiplier=1000, wait_exponential_max=20000)
+        def download_source(link, path):
+            with open(path, 'wb') as fp:
+                fp.write(requests.get(link, headers=self.headers, timeout=10).content)
+
         logger.info(f'====={_item["desc"]}, 开始下载!=====')
         if _item['type'] == 'photo':
             flag = 0
             for url in _item['link']:
                 path = f'{folder_path}/img/{_item["aweme_id"]}{"aa" + str(flag)}.jpg'
-                with open(path, 'wb') as fp:
-                    fp.write(requests.get(url, headers=self.headers, timeout=10).content)
+                try:
+                    download_source(url, path)
+                except requests.RequestException:
+                    logger.error(f'下载资源: {url}错误，超过五次，请手动重试. -->{_item}')
+                    continue
                 flag += 1
             logger.info(f'*****{_item["desc"]}, 下载完成!*****')
             return True
         else:
             path = f'{folder_path}/video/{_item["aweme_id"]}.mp4'
-            with open(path, 'wb') as fp:
-                fp.write(requests.get(_item['link'], headers=self.headers, timeout=10).content)
+            try:
+                download_source(_item['link'], path)
+            except requests.RequestException:
+                logger.error(f'下载资源: {_item["link"]}错误，超过五次，请手动重试. --> {_item}')
+                return False
             logger.info(f'*****{_item["desc"]}, 下载完成!*****')
             return True
 
     # 在字典中搜索关键字，返回信息，可以搜索到字典中所有匹配的关键字
     @staticmethod
-    def search_dir(_items, search_key):
+    def __search_dir(_items, search_key):
         stack = [_items]
         while stack:
             current_item = stack.pop()
@@ -321,7 +397,7 @@ class Douyin:
 
     # 获取帖子的内容
     @staticmethod
-    def get_post(post):
+    def __get_post(post):
         aweme_id = post.get('aweme_id')
         desc = post.get('desc')
         create_time = post.get('create_time')
@@ -336,7 +412,6 @@ class Douyin:
         elif types == 101:
             return
         else:
-            source = types
             logger.error(f'出现未知帖子类型: {post}')
             return
         video_tag = [tag['tag_name'] for tag in post.get('video_tag')] if post.get('video_tag') else None
@@ -365,7 +440,7 @@ class Douyin:
 
     # 获取用户信息
     @staticmethod
-    def get_user_info(user):
+    def __get_user_info(user):
         city = str(user.get('city'))
         country = str(user.get('country'))
         district = str(user.get('district'))
@@ -388,7 +463,7 @@ class Douyin:
 
     # 获取评论内容
     @staticmethod
-    def get_comment(c):
+    def __get_comment(c):
         sticker = None
         if c.get('sticker'):
             if c.get('sticker').get('animate_url'):
@@ -412,14 +487,14 @@ class Douyin:
         }
 
     @staticmethod
-    def __sleep_time(t: Union[int, float] = 5):
+    def __sleep_time(t: Union[int, float] = 1.5):
         time.sleep(t)
 
 
 if __name__ == '__main__':
     dou = Douyin()
     # 保存帖子评论
-    # dou.save2csv('7260706141649390904')
+    dou.save_comment_to_csv_by_id('7260706141649390904')
     # 获取帖子评论
     # items = dou.get_comment_by_id('7260706141649390904')
     # 获取热搜
