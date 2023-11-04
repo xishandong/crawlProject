c8ae8b4d3f25577583858764a8fd8482eba1b2bb
xishandong <xishandong296@gmail.com>
2023-11-04 15:57:52 +0800

    更新爬虫轮子的表格打印以及修复了一个bug

--- 进阶篇/爬虫轮子/crawl.py
+++ 进阶篇/爬虫轮子/crawl.py
@@ -1,21 +1,44 @@
 import csv
 import json
+import random
+import time
 from concurrent.futures import ThreadPoolExecutor
 from threading import Lock
 from typing import Literal, Union, Iterator
 
 import requests
 from loguru import logger
+from prettytable import PrettyTable
 from retrying import retry
+
+COLORS = {
+    'black': '30',
+    'red': '31',
+    'green': '32',
+    'yellow': '33',
+    'blue': '34',
+    'magenta': '35',
+    'cyan': '36',
+    'white': '37',
+    'reset': '0',
+    'bright_black': '90',
+    'bright_red': '91',
+    'bright_green': '92',
+    'bright_yellow': '93',
+    'bright_blue': '94',
+    'bright_magenta': '95',
+    'bright_cyan': '96',
+    'bright_white': '97',
+}
 
 
 class Log:
     log = logger
 
-    # # 配置日志信息
+    # 配置日志信息
     # log.add(
-    #     f"log_{1}.log",  # 日子文件名，后留一个占位符
-    #     rotation='1 MB',  # 日志分割，可以根据时间也可以根据大小
+    #     "log.log",  # 日子文件名，后留一个占位符
+    #     rotation='10 MB',  # 日志分割，可以根据时间也可以根据大小
     #     colorize=False,  # 是否染色
     #     level='DEBUG'  # 日志等级
     # )
@@ -41,9 +64,6 @@ log = Log()
 
 
 class Request_obj:
-    """
-    对requests的二次封装
-    """
     def __init__(
             self,
             url: str,
@@ -64,6 +84,7 @@ class Request_obj:
         self.request_args = {
             'headers': self.headers,
             'params': self.params,
+            'data': self.data,
             'cookies': self.cookies,
             'proxys': self.proxys
         }
@@ -73,8 +94,7 @@ class Request_obj:
                 -------------- request for ----------------
                 url  = {self.url}
                 method = {self.method}
-                args = {self.request_args}
-                """
+                args = {self.request_args}"""
 
 
 class ErrorResponse(Exception):
@@ -103,6 +123,9 @@ class CrawlBase(object):
     # 线程锁相关
     file_lock = Lock()
 
+    def __init__(self, sleep_interval: float = .5):
+        self.control_sleep_time: float = sleep_interval
+
     @retry(
         stop_max_attempt_number=5,
         wait_exponential_multiplier=5000,
@@ -114,12 +137,6 @@ class CrawlBase(object):
             request_obj: Request_obj,
             session: bool = False
     ) -> requests.Response:
-        """
-        发送请求，默认重试5次，此函数是私有函数，默认无法在实例调用
-        :param request_obj: 请求对象
-        :param session: 是否使用session
-        :return: response
-        """
         if session:
             method_pro = self.session_method_processor[request_obj.method.lower()]
         else:
@@ -127,34 +144,32 @@ class CrawlBase(object):
         log.debug(
             f'开始发送请求 {request_obj}'
         )
-        resp = method_pro(
-            url=request_obj.url,
-            headers=request_obj.headers,
-            params=request_obj.params,
-            data=json.dumps(request_obj.data, ensure_ascii=False, separators=(',', ':')).encode(),
-            cookies=request_obj.cookies,
-            proxies=request_obj.proxys
-        )
+        if request_obj.method.lower() == 'post':
+            resp = method_pro(
+                url=request_obj.url,
+                headers=request_obj.headers,
+                params=request_obj.params,
+                data=json.dumps(request_obj.data, ensure_ascii=False, separators=(',', ':')).encode(),
+                cookies=request_obj.cookies,
+                proxies=request_obj.proxys
+            )
+        else:
+            resp = method_pro(
+                url=request_obj.url,
+                headers=request_obj.headers,
+                params=request_obj.params,
+                cookies=request_obj.cookies,
+                proxies=request_obj.proxys
+            )
         if not self.after_request(request_obj, resp):
             log.error(f'收到的响应数据不符合期望，抛出错误，重试...{request_obj}')
             raise ErrorResponse('用户主动抛弃当前请求')
         return resp
 
     def before_request(self, request_obj: Request_obj) -> None:
-        """
-        此函数是在发起请求前一定会访问的，可以对请求对象进行修改
-        :param request_obj: 发起请求的对象
-        :return:
-        """
         ...
 
     def after_request(self, request_obj: Request_obj, response: requests.Response) -> bool:
-        """
-        此函数是收到响应后对响应进行校验，传入请求对象以及响应对象，如果返回false则会抛弃响应。对应返回请求体对象
-        :param request_obj: 请求对象
-        :param response: 响应对象
-        :return:
-        """
         return True
 
     def do_request(
@@ -168,22 +183,8 @@ class CrawlBase(object):
             proxys=None,
             session: bool = False,
             middleware=None,
-            is_abandon: bool = False
+            is_abandon: bool = False,
     ) -> Union[requests.Response, Request_obj, None]:
-        """
-        对requests进行二次封装，可以自定义中间件，以及是否抛弃此次请求
-        :param url: url
-        :param method: 方法
-        :param headers: 请求头
-        :param params: 请求参数
-        :param data: 请求json数据
-        :param cookies: cookie
-        :param proxys: 代理
-        :param session: 是否使用session
-        :param middleware: 中间件。可接受list[func]以及func
-        :param is_abandon: 是否抛弃本次请求
-        :return: 错误返回None，准确返回response，抛弃返回request_obj
-        """
         if proxys is None:
             proxys = {}
         if cookies is None:
@@ -215,22 +216,22 @@ class CrawlBase(object):
             log.debug(f'主动抛弃请求，返回请求对象...{request_obj}')
             return request_obj
         try:
+            time.sleep(self.control_sleep_time)
             resp = self.__ajax_requests(
                 request_obj=request_obj,
                 session=session
             )
             return resp
         except Exception as e:
-            logger.exception(e)
+            log.exception(e)
             return None
 
-    def save_item_to_csv(self, items: Iterator[dict], file_path: str = 'data.csv') -> None:
-        """
-        批量下载数据到csv中。使用多线程下载
-        :param items: 产生数据的生成器对象。需要是下载数据的dict格式
-        :param file_path: 保存文件路径
-        :return:
-        """
+    def save_item_to_csv(
+            self,
+            items: Iterator[dict],
+            file_path: str = 'data.csv',
+            max_workers: int = 30
+    ) -> None:
         def write_items(item: dict, is_write_headers: bool):
             header = list(item.keys())
             mode = 'a' if not is_write_headers else 'w'
@@ -239,11 +240,11 @@ class CrawlBase(object):
                     writer = csv.DictWriter(fp, header)
                     if is_write_headers:
                         writer.writeheader()
-                        log.info(f'写入表头了: {header}')
+                        log.info(f'写入表头了: {header}, 文件路径: {file_path}')
                     writer.writerow(item)
 
         is_first = True
-        with ThreadPoolExecutor(max_workers=100) as executor:
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
             for num, save_item in enumerate(items, start=1):
                 executor.submit(write_items, save_item, is_first)
                 if is_first:
@@ -266,3 +267,86 @@ class CrawlBase(object):
             elif isinstance(current_item, list):
                 for value in current_item:
                     stack.append(value)
+
+    @staticmethod
+    def format_table_print(
+            items: list[dict],
+            title: str = None,
+            column_names: list = None, *,
+            title_color: str = None,
+            header_color: str = None,
+            column_color: str = None,
+            random_title_color: bool = False,
+            random_header_color: bool = False,
+            random_column_color: bool = False,
+    ) -> None:
+        try:
+            assert items and isinstance(items, list) and isinstance(items[0], dict), ValueError(
+                "items必须是列表包含字典")
+        except AssertionError as e:
+            log.exception(e)
+            raise ValueError('0')
+
+        # 给文字染色
+        def dye_text(text: str, color: str = None, is_random: bool = False) -> str:
+            keys = list(COLORS.keys())
+            if is_random:
+                color = random.choice(keys)
+            if color in COLORS:
+                return f'\033[1;{COLORS[color]}m{text}\033[0m'
+            return text
+
+        # 获取最长的键值
+        max_key = [len(list(col.keys())) for col in items]
+        max_key_size = max(max_key)
+        # 获取key用做表格
+        item_keys = list(items[max_key.index(max_key_size)].keys())
+        column_list = [[] for _ in range(max_key_size)]
+        for index, key in enumerate(item_keys):
+            for item in items:
+                column_list[index].append(item.get(key, ''))
+        if not column_names:
+            column_names = item_keys
+
+        try:
+            assert len(column_names) == len(item_keys), ValueError('表头长度与数据最长项不相符')
+        except AssertionError as e:
+            log.exception(e)
+            raise ValueError('0')
+
+        # 对表格进行一些初始化
+        table = PrettyTable()
+        if title:
+            table.title = dye_text(title, title_color, random_title_color)
+
+        table.add_column(dye_text('序号', header_color),
+                         [dye_text(str(i), column_color, random_column_color) for i in
+                          range(1, len(column_list[0]) + 1)])
+
+        for j, column in enumerate(column_list, start=0):
+            col = [dye_text(i, column_color, random_column_color) for i in column]
+            table.add_column(dye_text(column_names[j], header_color, random_header_color), col)
+        print(table)
+
+
+if __name__ == '__main__':
+    c = CrawlBase()
+    test_list_dict = [
+        {
+            'name': 'test',
+            'arg': 18,
+            'interest': 'coding'
+        },
+        {
+            'name': 'test1',
+            'arg': 19,
+        },
+        {
+            'name': 'test2',
+            'arg': 20,
+            'interest': 'coding',
+            'hobby': 'singing',
+            'value': 'test'
+        }
+    ]
+    c.format_table_print(items=test_list_dict, column_color='green', random_column_color=True, random_header_color=True)
